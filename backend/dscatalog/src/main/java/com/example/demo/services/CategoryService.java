400328fc7289da8ce01cf093837b4c3a25181eb6
FelipeFariasFCF <felipe.fariasfcf@outlook.com>
2022-10-13 13:45:00 -0400

    Dto and database seeding

--- backend/dscatalog/src/main/java/com/example/demo/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/example/demo/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.CategoryDTO;
 import com.example.demo.entities.Category;
 import com.example.demo.repositories.CategoryRepository;
 
@@ -14,7 +17,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
-	public List<Category> findAll(){
-		return categoryRepository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
+		List<Category> list =  categoryRepository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
